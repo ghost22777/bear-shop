1bc6023707d6c7db8d61cee9a849051d3073178c
ghost22777 <nndlion34@gmail.com>
2025-08-06 09:10:54 +0700

    Update: chỉnh tỷ lệ pet hiếm và fix bug

--- pets.js
+++ pets.js
@@ -1,26 +1,26 @@
 const petList = [
-  // Siêu khó (weight = 1)
-  { name: "Kitsune", img: "images/Kitsune.png", weight: 1, price: 40 },
-  { name: "Raccoon", img: "images/Raccoon.png", weight: 1, price: 12 },
-  { name: "DiscoBee", img: "images/DiscoBee.png", weight: 1, price: 12 },
+  // 🟣 Siêu hiếm (Legendary) - cực khó quay
+  { name: "Kitsune", img: "images/Kitsune.png", weight: 0.1, price: 40 },
+  { name: "Raccoon", img: "images/Raccoon.png", weight: 0.1, price: 12 },
+  { name: "DiscoBee", img: "images/DiscoBee.png", weight: 0.1, price: 12 },
 
-  // Khó (weight = 3)
-  { name: "Dragonfly", img: "images/Dragonfly.png", weight: 3, price: 6 },
+  // 🔵 Hiếm (Epic) - khó
+  { name: "Dragonfly", img: "images/Dragonfly.png", weight: 0.3, price: 6 },
   { name: "MimicOctopus", img: "images/MimicOctopus.png", weight: 3, price: 5 },
-  { name: "Spinosaurus", img: "images/Spinosaurus.png", weight: 3, price: 8 },
+  { name: "Spinosaurus", img: "images/Spinosaurus.png", weight: 0.3, price: 8 },
   { name: "T-Rex", img: "images/T-Rex.png", weight: 3, price: 4 },
-  { name: "Butterfly", img: "images/Butterfly.png", weight: 3, price: 8 },
-  { name: "FennecFox", img: "images/FennecFox.png", weight: 3, price: 9 },
+  { name: "Butterfly", img: "images/Butterfly.png", weight: 0.4, price: 8 },
+  { name: "FennecFox", img: "images/FennecFox.png", weight: 0.3, price: 9 },
 
-  // Trung bình (weight = 5)
-  { name: "CorruptedKitsune", img: "images/CorruptedKitsune.png", weight: 7, price: 2.5 },
-  { name: "RedFox", img: "images/RedFox.png", weight: 8, price: 1 },
+  // 🟢 Trung bình (Rare) - tỷ lệ vừa phải
+  { name: "CorruptedKitsune", img: "images/CorruptedKitsune.png", weight: 5, price: 2.5 },
+  { name: "RedFox", img: "images/RedFox.png", weight: 5, price: 1 },
   { name: "BloodOwl", img: "images/BloodOwl.png", weight: 5, price: 2 },
   { name: "Moon_Cat", img: "images/Moon_Cat.png", weight: 5, price: 2 },
-  { name: "Chicken_Zombie", img: "images/Chicken_Zombie_Icon.png", weight: 7, price: 2 },
+  { name: "Chicken_Zombie", img: "images/Chicken_Zombie.png", weight: 5, price: 2 },
   { name: "Brontosaurus", img: "images/Brontosaurus.png", weight: 5, price: 2 },
-  { name: "Queen_bee", img: "images/Queen_bee.png", weight: 4, price: 3 },
+  { name: "Queen_bee", img: "images/Queen_bee.png", weight: 5, price: 3 },
 
-  // Dễ (weight = 7 hoặc 10 nếu bạn muốn)
-  { name: "FrenchFryFerret", img: "images/FrenchFryFerret.png", weight: 3, price: 7 }
+  // 🟡 Phổ thông (Common) - dễ quay ra
+  { name: "FrenchFryFerret", img: "images/FrenchFryFerret.png", weight: 0.3, price: 1 }
 ];
